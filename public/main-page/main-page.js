c7d2973d38ece8c428e42232978f3a956a4641e3
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2021-05-25 20:23:22 +0200

    Move script

--- public/main-page/main-page.js
+++ public/main-page/main-page.js
@@ -1,2 +1,10 @@
-console.log("hello");
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('http://localhost:8000/getAll')
+    .then(response => response.json())
+    .then(object => loadHTML(object));
+});     
 
+function loadHTML(object) {
+    const title = document.querySelector('.title');
+    title.innerText += " " + object.data[0].username;
+}
